e158d449871d2ded345c7713caeb5d639e530168
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2021-12-04 11:07:39 -0600

    Add singleton methods for base converter

--- lib/algos/base_converter.rb
+++ lib/algos/base_converter.rb
@@ -2,20 +2,34 @@
 
 module Algos
   class BaseConverter
-    def initialize(radix:, symbols: nil)
+    BASE_TO_SYMBOLS = {
+      base62: {symbols: [*"0".."9", *"a".."z", *"A".."Z"], radix: 62},
+      binary: {symbols: ["0", "1"], radix: 2},
+      hexadecimal: {symbols: [*"0".."9", *"A".."F"], radix: 16}
+    }.freeze
+
+    BASE_TO_SYMBOLS.each do |base, values|
+      define_singleton_method(base) do
+        new(radix: values[:radix], symbols: values[:symbols])
+      end
+    end
+
+    def initialize(radix:, symbols:)
       @symbols = symbols
       @radix = radix
     end
 
+    attr_reader :symbols, :radix
+
     def value_from_decimal(decimal_value)
-      return @symbols.first if decimal_value.zero?
+      return symbols.first if decimal_value.zero?
 
       base_conversion_elements = []
       conversion_value = decimal_value
 
       while conversion_value.positive?
-        base_conversion_elements.push(@symbols[conversion_value % @radix])
-        conversion_value /= @radix
+        base_conversion_elements.push(symbols[conversion_value % radix])
+        conversion_value /= radix
       end
 
       base_conversion_elements.reverse.join
--- spec/lib/algos/base_converter_spec.rb
+++ spec/lib/algos/base_converter_spec.rb
@@ -6,7 +6,7 @@ describe Algos::BaseConverter do
   describe "#value_from_decimal" do
     context "custom base 62" do
       it "generates a base62 based on a a given value" do
-        base62_converter = Algos::BaseConverter.new(symbols: [*"0".."9", *"a".."z", *"A".."Z"], radix: 62)
+        base62_converter = Algos::BaseConverter.base62
 
         expect(base62_converter.value_from_decimal(0)).to(eq("0"))
         expect(base62_converter.value_from_decimal(125)).to(eq("21"))
@@ -15,31 +15,30 @@ describe Algos::BaseConverter do
     end
 
     context "binary" do
-      let(:binary_symbols) { %w[0 1] }
       it "sets 0, 1 and 10 to their base 2 equivalents" do
-        base2_converter = Algos::BaseConverter.new(symbols: binary_symbols, radix: 2)
+        base2_converter = Algos::BaseConverter.binary
         expect(base2_converter.value_from_decimal(0)).to(eq("0"))
         expect(base2_converter.value_from_decimal(1)).to(eq("1"))
         expect(base2_converter.value_from_decimal(10)).to(eq("1010"))
       end
 
       it "converts unsigned short integer max to binary" do
-        expect(Algos::BaseConverter.new(symbols: binary_symbols, radix: 2).value_from_decimal(65_535)).to(eq("1111111111111111"))
+        expect(Algos::BaseConverter.binary.value_from_decimal(65_535)).to(eq("1111111111111111"))
       end
     end
 
-    context "hexidecimal" do
+    context "hexadecimal" do
       let(:hex_symbols) { [*"0".."9", *"A".."F"] }
 
       it "converts 1 through 10 to their base16 equivalent" do
-        base_converter = Algos::BaseConverter.new(symbols: hex_symbols, radix: 16)
+        base_converter = Algos::BaseConverter.hexadecimal
         (0..10).each_with_index do |base10_value, index|
           expect(base_converter.value_from_decimal(base10_value)).to(eq(hex_symbols[index]))
         end
       end
 
       it "converts unsigned short integer max to hex" do
-        expect(Algos::BaseConverter.new(symbols: hex_symbols, radix: 16).value_from_decimal(65_535)).to(eq("FFFF"))
+        expect(Algos::BaseConverter.hexadecimal.value_from_decimal(65_535)).to(eq("FFFF"))
       end
     end
 
